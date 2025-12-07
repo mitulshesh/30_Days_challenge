4646610e3f83fa477ade1594dce03c9c7500bb71
mitulshesh <mitul.shesh28@gmail.com>
2025-12-07 20:02:44 +0530

    30 days challenege.. ninth push

--- src/ChatgptInteractive/arrays/TrappingRainWater.java
+++ src/ChatgptInteractive/arrays/TrappingRainWater.java
@@ -3,7 +3,8 @@ package ChatgptInteractive.arrays;
 public class TrappingRainWater {
 
     public static void main(String[] args) {
-
+        int[] bars = {4,2,0,3,2,5};
+        System.out.println(maxAreaTrappingRainWater(bars));
     }
 
     public static int maxAreaTrappingRainWater(int[] bars){
@@ -11,9 +12,30 @@ public class TrappingRainWater {
         int leftMax=0;
         int rightMax=0;
 
-        int start = 0;
+        int left = 0;
+        int right=bars.length-1;
+        int totalWater=0;
 
+        while(left < right){
 
-        return 0;
+            if(bars[left] < bars[right]) {
+
+                if (leftMax > bars[left]) {
+                    totalWater += leftMax - bars[left];
+                } else {
+                    leftMax = bars[left];
+                }
+                left++;
+            }else {
+
+                if (rightMax > bars[right]) {
+                    totalWater += rightMax - bars[right];
+                } else {
+                    rightMax = bars[right];
+                }
+                right--;
+            }
+        }
+        return totalWater;
     }
 }
