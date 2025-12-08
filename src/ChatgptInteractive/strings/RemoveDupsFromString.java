9fd489028f86b75ae6d77876cd00b73e7a41bb09
Mitul Shesh <mitul.shesh28@gmail.com>
2025-12-08 08:58:21 +0530

    Eight push

--- src/ChatgptInteractive/strings/RemoveDupsFromString.java
+++ src/ChatgptInteractive/strings/RemoveDupsFromString.java
@@ -1,5 +1,6 @@
 package ChatgptInteractive.strings;
 
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class RemoveDupsFromString {
@@ -14,7 +15,8 @@ public class RemoveDupsFromString {
         //mapToObj(c -> String.valueOf((char)c)) → converts int → character → string
         //Collectors.joining() → joins characters back into a single string
 
-       String processedString = s.chars().distinct().mapToObj(c -> String.valueOf((char)c)).collect(Collectors.joining());
+       //String processedString = s.chars().distinct().mapToObj(c -> String.valueOf((char)c)).collect(Collectors.joining());
+        String processedString = Arrays.stream(s.split("")).distinct().collect(Collectors.joining());
        return processedString;
     }
 }
